3ef20d6fa4122c1dc7fc9dff53d860d517f06f6d
Jacob Dalton <jacobrdalton@gmail.com>
2014-01-09 21:44:47 -0600

    fixed panning; added loading gif; other stuff

--- client.js
+++ client.js
@@ -10,20 +10,23 @@ L.tileLayer('http://{s}.tile.cloudmade.com/{key}/{styleId}/256/{z}/{x}/{y}.png',
   styleId: 22677
 }).addTo(map);
 L.Icon.Default.imagePath = 'images';
+var markers = [];
 
 /*** UI Functions ***/
+// Normally I would use angular or jquery to do the event binding and DOM
+// updating but this was relatively simple so I went with native DOM for this app.
 
 // Use anonymous functions for callbacks to capture request data within
 // closures.
 window.doSearch = function() {
   var query = document.querySelector('#map-search').value;
+  showLoadingMessage();
   request.get('twitter/search')
     .query({q: query})
     .end( function(error, res) {
       var twitterData = JSON.parse(res.body);
-      if(!twitterData.statuses || twitterData.statuses.length == 0) {
-        // TODO: handle error
-        console.log('twitter did not return any statuses');
+      if(error || !twitterData.statuses || twitterData.statuses.length == 0) {
+        showErrorMessage(error);
         return;
       }
       buildSearchTweets(twitterData.statuses, query);
@@ -31,15 +34,25 @@ window.doSearch = function() {
 }
 
 window.lookupTimeline = function(idString, screenName) {
+  showLoadingMessage();
   currentTimelineUserName = screenName;
   request.get('twitter/timeline/' + idString)
     .end(function( error, res) {
-      if( !error && res.body) {
-        // TODO: handle error
-        var tweets = JSON.parse(res.body);
-        buildTimelineTweets(tweets, screenName);
+      if(error || !res.body) {
+        showErrorMessage(error);
+        return;
       }
+      var tweets = JSON.parse(res.body);
+      buildTimelineTweets(tweets, screenName);
     });
+}
+
+window.panTo = function(markerIndex) {
+  var marker = markers[markerIndex];
+  if(marker != null) {
+    marker.openPopup();
+    map.panTo(marker.getLatLng());
+  }
 }
 
 window.backToTweets = function() {
@@ -63,12 +76,35 @@ function buildTimelineTweets(tweets, screenName) {
 
 function buildTweets(tweetList, isSearch) {
   var tweetHtml = "";
-  tweetList.forEach( function(item) {
-    tweetHtml += tweetTemplate({ tweet: item, is_search: isSearch});
+  destroyMarkers();
+  tweetList.forEach( function(tweet) {
+    var markerIndex;
+    if(tweet.geo) {
+      var marker = L.marker(tweet.geo.coordinates).bindPopup(tweet.text).addTo(map).openPopup();
+      markerIndex = markers.push(marker) - 1;
+      console.log(markerIndex);
+    }
+    tweetHtml += tweetTemplate({ tweet: tweet, is_search: isSearch, marker_index: markerIndex});
   });
   return tweetHtml;
 }
 
+function destroyMarkers() {
+  markers.forEach( function(item) {
+    map.removeLayer(item);
+    item = null;
+  });
+  markers = [];
+}
+
+function showLoadingMessage() {
+  tweetContainer.innerHTML = '<h2>Loading</h2><img id="loader" src="images/loading.gif" />';
+}
+
+function showErrorMessage(error) {
+  console.log(error);
+  tweetContainer.innerHTML = "<h2>No Results.</h2>";
+}
 
 // TODO: refactor to a single getTemplate method with consistent addressing.
 
